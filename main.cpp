f795b51831f9617ab3018874c3b0f98219d782d8
Ryan Romero <50720051+RomeroRyan@users.noreply.github.com>
2019-05-25 16:20:20 -0700

    Update with ASM functions (not debugged yet!)

--- main.cpp
+++ main.cpp
@@ -38,19 +38,76 @@ void printMenu(){
   cout << "Please enter your choice: ";
 }
 
-/*
-void sortArray(){
-    //CODE: use asm
-   __asm{}
-}
-*/
 
+void sortArray(int myArray[]){
+   __asm( "AssemblyCode"){
+      push  eax       //
+      push  ebx       // Saves any info in these regs.
+      push  ecx       //
+      push  esi       // regs will be use during the function
+      push  edi       //
 
-/*
-void reverseArray(){
-    //CODE: use asm
+      mov   ecx, LENGTHOF myArray                 // counter
+      mov   esi, OFFSET myArray                   // ptr to first element in array
+      Loop1:
+          mov   ebx, [esi]                  // saves element x into EBX
+          mov   edi, esi                    // copy ptr to first element to EDI
+          mov   eax, ecx                    // copy counter to EAX
+          push  ecx                         // Saves counte
+          mov   ecx, eax                    // ECX and EAX = counter
+      Loop2:
+          add   edi, TYPE myArray           // EDI now points to next element [ x + 1 ]
+          cmp   ebx, [edi]                  // Compare element [ X ] and [ x+1 ] in myArray
+          jge   SkipCode                    // IF [x] > [x+1], skips following lines of code
+          mov   ebx, [edi]                  // EBX, now holds what was the next element (moving up array)
+          mov   eax, ecx                    // EAX = current counter
+          sub   eax, TYPE myArray           // counter--
+      SkipCode:
+          loop  Loop2
+          mov   edi, OFFSET myArray + SIZEOF myArray  //
+          sub   edi, eax
+          xchg  [esi], ebx
+          mov   [edi], ebx
+          add   esi, TYPE myArray
+          pop   ecx
+          loop  Loop1
+
+      pop   edi       // Pops all regs that were use
+      pop   esi       //
+      pop   ecx       //
+      pop   ebx
+      pop   eax
+   };
 }
-*/
+
+void reverseArray(int myArray[]){
+    __asm( "AssemblyCode"){
+        push  eax
+        push  ebx
+        push  ecx
+        push  edi
+        push  esi
+
+        mov   ecx, LENGTHOF myArray / 2    // loop counter
+        mov   esi, OFFSET myArray                                   // Ptr to first element
+        mov   edi, OFFSET myArray + SIZEOF myArray - TYPE myArray   // Ptr to last element
+
+        myLoop:
+            mov   eax, [esi]    //move element to reg
+            mov   ebx, [edi]    //move element to reg
+            mov   [esi], ebx    //swap
+            mov   [edi], eax    //swap
+            add   esi, TYPE myArray   //inc
+            sub   edi, TYPE myArray   //dec
+            loop myLoop
+
+        pop   esi
+        pop   edi
+        pop   ecx
+        pop   ebx
+        pop   eax
+}
+
 
 // ANY OTHER FUNCTIONS WISH TO INCLUDE HERE
 
@@ -77,7 +134,6 @@ int main()
           validSize = false;
       }
   }while (validSize == false);
-  delete validSize;
 
 /**************** ASK USER TO INPUT INTS INTO ARRAY ***************************/
 
@@ -87,7 +143,7 @@ int main()
 
   cout << endl;
   cout << "Inputs saved. Current array: ";
-  for (counter = 0; counter < arraySize; counter++){
+  for (int counter = 0; counter < arraySize; counter++){
       cout << myArray[counter] << " ";
   }
   cout << endl;
@@ -102,20 +158,27 @@ int main()
       switch (userChoice)
       {
           case 1:
-              //CODE sortArray()
-          case 2:
-              //CODE reverseArray()
+              sortArray(myArray);
+              //***TEST***
+              cout << "Current Array: ";
+              for(int i = 0; i < arraySize; i++)
+                  {cout << myArray[i] << " ";}
+              break;
 
-          // . . . .
+          case 2:
+              reverseArray(myArray);
+              break;
+
+          case 9:
+              //do nothin
+              break;
 
           default:
       			cout << "Invalid entry." << endl;
       			break;
       		}
-      } while (userChoice != // NUM TO EXIT CODE)
+      } while (userChoice != 9);
 
       cout << "Exiting Program." << endl;
-
-  }
 return 0;
 }
